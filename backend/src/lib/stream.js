145868a680cd49d31600a0d2a20bd8c70314d04a
anirudhaiysola <anirudhisola1011@gmail.com>
2026-03-17 17:51:22 -0500

    stream auth sync

---
+++ backend/src/lib/stream.js
@@ -0,0 +1,29 @@
+import { StreamChat } from "stream-chat";
+import { ENV } from "./env.js";
+
+const apiKey = ENV.STREAM_API_KEY;
+const apiSecret = ENV.STREAM_API_SECRET;
+
+if (!apiKey || !apiSecret) {
+  throw new Error("Stream API key or secret are required");
+}
+
+export const chatClient = StreamChat.getInstance(apiKey, apiSecret);
+
+export const upsertStreamUser = async (userData) => {
+  try {
+    await chatClient.upsertUser(userData);
+    return userData;
+  } catch (error) {
+    console.error("Error upserting Stream user:", error);
+  }
+};
+
+export const deleteStreamUser = async (userId) => {
+  try {
+    await chatClient.deleteUser(userId);
+    console.log(`Stream user ${userId} deleted successfully`);
+  } catch (error) {
+    console.error("Error deleting Stream user:", error);
+  }
+};
