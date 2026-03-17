8be93eb25c0bd35e97a8c434176d2930d1332c16
anirudhaiysola <anirudhisola1011@gmail.com>
2026-03-17 18:29:51 -0500

    server side auth

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
 import { serve } from "inngest/express";
+import { clerkMiddleware } from "@clerk/express";
 
 import { ENV } from "./lib/env.js";
 import path from "path";
 import { connectDB } from "./lib/db.js";
 import { inngest, functions } from "./lib/inngest.js";
+import chatRoutes from "./routes/chatRoutes.js";
 
 const app = express();
 
@@ -14,14 +16,12 @@ const __dirname = path.resolve();
 //middleware
 app.use(express.json());
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(clerkMiddleware());
 app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/chat", chatRoutes);
 
 app.get("/health", (req, res) => {
   res.status(200).json({ message: "api is up and running!" });
-});
-
-app.get("/books", (req, res) => {
-  res.status(200).json({ message: "books api is up and running!" });
 });
 
 // make our app ready for deployment
