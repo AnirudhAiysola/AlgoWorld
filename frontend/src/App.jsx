d47211572956dd37bcddc0d72091b068b87996e8
anirudhaiysola <anirudhisola1011@gmail.com>
2026-03-24 11:02:17 -0500

    home page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,42 @@
-import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/react";
+import { Show, UserButton, useUser } from "@clerk/react";
 import { Navigate, Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
 import ProblemsPage from "./pages/ProblemsPage";
+import DashboardPage from "./pages/DashboardPage";
 import { Toaster } from "react-hot-toast";
 
 function App() {
+  const { isLoaded } = useUser();
+  if (!isLoaded) return null;
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/"
+          element={
+            <>
+              <Show when="signed-out">
+                <HomePage />
+              </Show>
+              <Show when="signed-in">
+                <Navigate to="/dashboard" />
+              </Show>
+            </>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <>
+              <Show when="signed-in">
+                <DashboardPage />
+              </Show>
+              <Show when="signed-out">
+                <Navigate to="/" />
+              </Show>
+            </>
+          }
+        />
         <Route
           path="/problems"
           element={
@@ -16,7 +44,7 @@ function App() {
               <Show when="signed-in">
                 <ProblemsPage />
               </Show>
-              <Show when="not-signed-in">
+              <Show when="signed-out">
                 <Navigate to="/" />
               </Show>
             </>
